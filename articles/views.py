aab6a5757f3532ca2504f876efef4888ab8bc52c
sego <sego.maris@gmail.com>
2018-02-28 12:02:37 +0100

    model created

--- articles/views.py
+++ articles/views.py
@@ -2,12 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    articles = [
-        {'id': 1, 'title': 'First article', 'body': 'This is my first article'},
-        {'id': 2, 'title': 'Second article', 'body': 'This is my second article'},
-        {'id': 3, 'title': 'Third article', 'body': 'This is my third article'},
-    ]
-    return render(request, 'articles/index.html', {'articles': articles})
+    return render(request, 'articles/index.html', {})
 
 def detail(request, id):
     return render(request, 'articles/detail.html', {'id': id})
