ab6844ad2ab8a52543e5ff5212ee934f630b86f0
sego <sego.maris@gmail.com>
2018-02-28 18:37:02 +0100

    instructions

--- articles/views.py
+++ articles/views.py
@@ -6,3 +6,15 @@ from django.utils import timezone
 def index(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'articles/index.html', {'posts':posts})
+
+def detail(request):
+    pass
+
+def delete(request):
+    pass
+
+def create(request):
+    pass
+
+def modify(request):
+    pass
